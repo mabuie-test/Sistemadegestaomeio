f5bbbc69e5eac1a33b1afda3fcc53c90bc13bf77
mabuie-test <jorgemaabuie@gmail.com>
2025-05-25 23:14:07 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,30 +3,54 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Navbar from './components/Navbar';
 import ProtectedRoute from './components/ProtectedRoute';
+import ProtectedLayout from './components/ProtectedLayout';
+
 import LoginPage from './pages/LoginPage';
 import Dashboard from './pages/Dashboard';
 import UsersAdminPage from './pages/UsersAdminPage';
+import ContainersAdminPage from './pages/ContainersAdminPage';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Navbar />
+
         <Routes>
           <Route path="/login" element={<LoginPage />} />
+
+          {/* Dashboard (qualquer utilizador autenticado) */}
           <Route
             path="/"
             element={
               <ProtectedRoute>
-                <Dashboard />
+                <ProtectedLayout>
+                  <Dashboard />
+                </ProtectedLayout>
               </ProtectedRoute>
             }
           />
+
+          {/* Gestão de Contentores (apenas admin) */}
+          <Route
+            path="/containers"
+            element={
+              <ProtectedRoute adminOnly>
+                <ProtectedLayout>
+                  <ContainersAdminPage />
+                </ProtectedLayout>
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Gestão de Utilizadores (apenas admin) */}
           <Route
             path="/users"
             element={
               <ProtectedRoute adminOnly>
-                <UsersAdminPage />
+                <ProtectedLayout>
+                  <UsersAdminPage />
+                </ProtectedLayout>
               </ProtectedRoute>
             }
           />
