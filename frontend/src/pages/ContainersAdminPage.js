26c9244c9dea0b81bfc330ae698fd8bce37c6fe3
mabuie-test <jorgemaabuie@gmail.com>
2025-05-25 23:25:43 +0200

    Create ContainersAdminPage.js

---
+++ frontend/src/pages/ContainersAdminPage.js
@@ -0,0 +1,11 @@
+// frontend/src/pages/ContainersAdminPage.js
+import React from 'react';
+import AdminContainers from '../components/AdminContainers';
+
+export default function ContainersAdminPage() {
+  return (
+    <div className="page containers-admin-page">
+      <AdminContainers />
+    </div>
+  );
+}
