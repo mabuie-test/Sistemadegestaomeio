0220007776de74bade11d392bb08f568d82e93b2
mabuie-test <jorgemaabuie@gmail.com>
2025-05-25 17:34:56 +0200

    Create UsersAdminPage.js

---
+++ frontend/src/pages/UsersAdminPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AdminUsers from '../components/AdminUsers';
+
+export default function UsersAdminPage() {
+  return (
+    <div className="page users-admin-page">
+      <AdminUsers />
+    </div>
+  );
+}
