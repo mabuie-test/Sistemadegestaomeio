e4666ba754b9e06bf5611dc198cba731b0f9cc6b
mabuie-test <jorgemaabuie@gmail.com>
2025-05-25 23:12:06 +0200

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,14 +4,17 @@ import { AuthContext } from '../context/AuthContext';
 
 export default function Navbar() {
   const { user, logout } = useContext(AuthContext);
-
-  // Se não estiver autenticado, não mostrar nada
   if (!user) return null;
 
   return (
     <nav className="navbar">
       <Link to="/">Dashboard</Link>
-      {user.role === 'admin' && <Link to="/users">Gestão de Utilizadores</Link>}
+      {user.role === 'admin' && (
+        <>
+          <Link to="/containers">Gestão Contentores</Link>
+          <Link to="/users">Gestão Utilizadores</Link>
+        </>
+      )}
       <button onClick={logout}>Sair</button>
     </nav>
   );
